a859ebdff296d77dfcb23665bd1d3ba5deccde27
Ramis Yuksel <ramisyk97@gmail.com>
2022-03-29 23:31:24 +0300

    Add project files.

---
+++ OrderDomain/AggregateModels/BuyerModels/Buyer.cs
@@ -0,0 +1,15 @@
+﻿using Order.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Domain.AggregateModels.BuyerModels
+{
+    public class Buyer : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ OrderDomain/AggregateModels/OrderModels/Order.cs
@@ -0,0 +1,47 @@
+﻿using Order.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Domain.AggregateModels.OrderModels
+{
+    public class Order : BaseEntity, IAggregateRoot
+    {
+        public DateTime OrderDate { get; private set; }
+        public string Description { get; private set; }
+        public int BuyerId { get; private set; }
+        public string OrderStatus { get; private set; }
+        public Address Address { get; private set; }
+
+        public ICollection<OrderItem> OrderItems { get; private set; }
+
+
+        public Order(DateTime orderDate, string description, int buyerId, string orderStatus, Address address, ICollection<OrderItem> orderItems)
+        {
+            if (orderDate < DateTime.Now)
+            {
+                throw new Exception("Order Date must be greater than now");
+            }
+
+            if (address.City != "")
+            {
+                throw new Exception("City cannot be empty");
+            }
+
+            OrderDate = orderDate;
+            Description = description;
+            BuyerId = buyerId;
+            OrderStatus = orderStatus;
+            Address = address;
+            OrderItems = orderItems;
+        }
+
+        public void AddOrderItem(int quantity, decimal price, int productId)
+        {
+            OrderItem item = new(quantity, price, productId);
+            OrderItems.Add(item);
+        }
+    }
+}
